877ceb3e04ad7ecc669e306e8fd2ac02f6774ee9
victorproexp <82384771+victorproexp@users.noreply.github.com>
2023-01-03 14:22:03 +0100

    loki

--- auktionAPI/Services/BudHandler/BudHandler.cs
+++ auktionAPI/Services/BudHandler/BudHandler.cs
@@ -2,42 +2,42 @@ namespace auktionAPI.Services;
 
 public class BudHandler : IBudHandler
 {
+    private Bud bud = new();
+    
     public List<Auktion> AuktionList { get; set; } = new();
 
-    public BudHandler()
+    public Auktion? UpdateAuctionIfBidIsValid(Bud newBud)
     {
-    }
+        bud = newBud;
 
-    public Auktion? UpdateAuctionIfBidIsValid(Bud bud)
-    {
-        var auktion = GetAuctionIfBidIsValid(bud);
+        var auktion = GetAuctionIfBidIsValid();
         
         if (auktion is not null)
         {
-            InsertBidInAuction(bud, auktion);
+            InsertBidInAuction(auktion);
         }
 
         return auktion;
     }
 
-    private Auktion? GetAuctionIfBidIsValid(Bud bud)
+    private Auktion? GetAuctionIfBidIsValid()
     {
-        return AuktionList.FirstOrDefault(a => IsMatchOnVare(bud, a) && IsHighestBid(bud, a));
+        return AuktionList.FirstOrDefault(a => IsMatchOnVare(a) && IsHighestBid(a));
     }
 
-    private bool IsMatchOnVare(Bud newBud, Auktion auktion)
+    private bool IsMatchOnVare(Auktion auktion)
     {
-        return auktion.VareId!.Equals(newBud.VareId);
+        return auktion.VareId!.Equals(bud.VareId);
     }
 
-    private bool IsHighestBid(Bud newBud, Auktion auktion)
+    private bool IsHighestBid(Auktion auktion)
     {
-        return (newBud.Value > auktion.CurrentBud.Value);
+        return (bud.Value > auktion.CurrentBud.Value);
     }
 
-    private void InsertBidInAuction(Bud newBud, Auktion auktion)
+    private void InsertBidInAuction(Auktion auktion)
     {
-        auktion.BudList.Add(newBud);
-        auktion.CurrentBud = newBud;
+        auktion.BudList.Add(bud);
+        auktion.CurrentBud = bud;
     }
 }
